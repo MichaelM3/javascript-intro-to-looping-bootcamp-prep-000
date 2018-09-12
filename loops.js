6aa9086bb84d82e41521cf941fdf80cc75fd6d8c
Michael Muniz <michael.muniz94@gmail.com>
2018-09-12 02:57:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i === 0) {
-      return '';
-    }
-    else if (i === 1) {
+    if (i === 1) {
       myArray.push(`'I am ${i} strange loop'`);
     }
     else {
