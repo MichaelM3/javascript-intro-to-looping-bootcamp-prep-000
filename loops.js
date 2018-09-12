5abb9bbc977e44bac6500feb48f66b74203d68f5
Michael Muniz <michael.muniz94@gmail.com>
2018-09-12 02:57:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      myArray.push(`'I am ${i} strange loop'`);
+      array.push(`'I am ${i} strange loop'`);
     }
     else {
-      myArray.push(`'I am ${i} strange loops.'`);
+      array.push(`'I am ${i} strange loops.'`);
     }
     return myArray
   }
