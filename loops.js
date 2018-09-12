468657d48a924f13ddc0138fe65416cb82584b4c
Michael Muniz <michael.muniz94@gmail.com>
2018-09-12 04:07:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,7 +24,6 @@ function incrementVariable() {
 function doWhileLoop(array) {
   do {
     array.pop();
-  }
-  while (array.length > 0 && incrementVariable());
+  }while (array.length > 0 && incrementVariable());
   return array;
   }
